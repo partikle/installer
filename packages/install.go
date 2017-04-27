7978cd726759c7c4c9c799bcea4d3a33b9024083
ilackarms <sdw35@cornell.edu>
2017-04-26 21:08:37 -0400

    start work on fedora

---
+++ packages/install.go
@@ -0,0 +1,49 @@
+package packages
+
+import (
+	"fmt"
+	"github.com/ilackarms/pkg/errors"
+	"io"
+	"io/ioutil"
+	"os/exec"
+	"strings"
+)
+
+const (
+	ubuntu = "Ubuntu"
+	fedora = "Fedora"
+)
+
+func Install(out io.Writer) error {
+	versionData, err := getVersion()
+	if err != nil {
+		return errors.New("failed to determine versionData", err)
+	}
+	if strings.Contains(versionData, ubuntu) {
+		return installUbuntu(out)
+	}
+	if strings.Contains(versionData, fedora) {
+		return errors.New("fedora not yet supported", nil)
+	}
+	return errors.New("unknown distro type: "+string(versionData), nil)
+}
+
+func getVersion() (string, error) {
+	versionData, err := ioutil.ReadFile("/etc/os-release")
+	if err != nil {
+		return "", errors.New("failed to get /proc/version", err)
+	}
+	return string(versionData), nil
+}
+
+func command(out io.Writer, args ...string) error {
+	fmt.Fprintf(out, "running command: %v\n", args)
+	cmd := exec.Command(args[0])
+	cmd.Args = args
+	cmd.Stdout = out
+	cmd.Stderr = out
+	if err := cmd.Run(); err != nil {
+		return errors.New("failed running command "+strings.Join(args, " "), err)
+	}
+	return nil
+}
--- packages/ubuntu.go
+++ packages/ubuntu.go
@@ -1,42 +1,10 @@
 package packages
 
 import (
-	"fmt"
-	"github.com/ilackarms/pkg/errors"
 	"io"
-	"io/ioutil"
-	"os/exec"
-	"strings"
 )
 
-const (
-	ubuntu = "Ubuntu"
-	fedora = "Fedora"
-)
-
-func Install(out io.Writer) error {
-	versionData, err := getVersion()
-	if err != nil {
-		return errors.New("failed to determine versionData", err)
-	}
-	if strings.Contains(versionData, ubuntu) {
-		return installUbuntu(out)
-	}
-	if strings.Contains(versionData, fedora) {
-		return errors.New("fedora not yet supported", nil)
-	}
-	return errors.New("unknown distro type: "+string(versionData), nil)
-}
-
-func getVersion() (string, error) {
-	versionData, err := ioutil.ReadFile("/proc/version")
-	if err != nil {
-		return "", errors.New("failed to get /proc/version", err)
-	}
-	return string(versionData), nil
-}
-
-var deps = []string{
+var ubuntuPackages = []string{
 	"libxen-dev",
 	"curl",
 	"git",
@@ -52,21 +20,9 @@ func installUbuntu(out io.Writer) error {
 		"install",
 		"-y",
 	}
-	args = append(args, deps...)
+	args = append(args, ubuntuPackages...)
 	if err := command(out, args...); err != nil {
 		return err
-	}
-	return nil
-}
-
-func command(out io.Writer, args ...string) error {
-	fmt.Fprintf(out, "running command: %v\n", args)
-	cmd := exec.Command(args[0])
-	cmd.Args = args
-	cmd.Stdout = out
-	cmd.Stderr = out
-	if err := cmd.Run(); err != nil {
-		return errors.New("failed running command "+strings.Join(args, " "), err)
 	}
 	return nil
 }
