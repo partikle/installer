2ef0f8415a2d21d500129a3e7f405de3cea5df37
ilackarms <sdw35@cornell.edu>
2017-04-26 23:58:37 -0400

    install function for rump

--- main.go
+++ main.go
@@ -1,14 +1,45 @@
 package main
 
 import (
+	"flag"
 	"github.com/Sirupsen/logrus"
 	"github.com/partikle/installer/packages"
+	"github.com/partikle/installer/rump"
+	"path/filepath"
 )
 
 func main() {
+	workdir := flag.String("w", "", "directory to build rump in")
+	rumpdir := flag.String("o", "", "output directory for rump binaries")
+	platform := flag.String("p", "", "platform to build for. can be hw, xen, or both")
+	flag.Parse()
 	log := logrus.New()
+	for name, value := range map[string]string{
+		"workdir":  *workdir,
+		"rumpdir":  *rumpdir,
+		"platform": *platform,
+	} {
+		if value == "" {
+			log.Fatalf("%s must be provided using its given flag. see installer -h for help", name)
+		}
+	}
+	if *platform != "hw" && *platform != "xen" && *platform != "both" {
+		log.Fatal("platform must be one of the following, \"hw\", \"xen\", or\"both\"")
+	}
+	wd, err := filepath.Abs(*workdir)
+	if err != nil {
+		log.Fatalf("failed to find absolute path to %s", *workdir)
+	}
+	rd, err := filepath.Abs(*rumpdir)
+	if err != nil {
+		log.Fatalf("failed to find absolute path to %s", *rumpdir)
+	}
+
 	log.Info("Installing Partikle!")
 	if err := packages.Install(log.Writer()); err != nil {
+		log.Fatal(err)
+	}
+	if err := rump.BuildRump(log.Writer(), wd, rd, *platform); err != nil {
 		log.Fatal(err)
 	}
 	log.Info("done!")
---
+++ rump/buildrump.go
@@ -0,0 +1,72 @@
+package rump
+
+import (
+	"fmt"
+	"io"
+	"path/filepath"
+
+	"github.com/ilackarms/pkg/errors"
+	"github.com/partikle/installer/pkg/exec"
+)
+
+func BuildRump(out io.Writer, workdir, rumpdir, platform string) error {
+	fmt.Fprintf(out, "Building Rumprun for %s, using workdir %s. "+
+		"Binaries will be saved to %s\n", platform, workdir, rumpdir)
+	if err := exec.Command(out,
+		"git",
+		"clone",
+		"https://github.com/rumpkernel/rumprun",
+	).Dir(workdir).Run(); err != nil {
+		return err
+	}
+	if err := exec.Command(out,
+		"git",
+		"submodule",
+		"update",
+		"--init",
+	).Dir(filepath.Join(workdir, "rumprun")).Run(); err != nil {
+		return err
+	}
+	if platform == "both" {
+		if err := runBuildrumpSH(out, workdir, rumpdir, "hw"); err != nil {
+			return errors.New("failed installing for hw", err)
+		}
+		if err := runBuildrumpSH(out, workdir, rumpdir, "xen"); err != nil {
+			return errors.New("failed installing for hw", err)
+		}
+	} else {
+		if err := runBuildrumpSH(out, workdir, rumpdir, platform); err != nil {
+			return errors.New("failed installing for "+platform, err)
+		}
+	}
+	return nil
+}
+
+func runBuildrumpSH(out io.Writer, workdir, rumpdir, platform string) error {
+	buildRR := filepath.Join(workdir, "rumprun", "build-rr.sh")
+	if err := exec.Command(out,
+		buildRR,
+		"-d",
+		rumpdir,
+		"-o",
+		"./obj",
+		platform,
+		"build",
+		"--",
+		"",
+	).Dir(filepath.Join(workdir, "rumprun")).Run(); err != nil {
+		return err
+	}
+	if err := exec.Command(out,
+		buildRR,
+		"-d",
+		rumpdir,
+		"-o",
+		"./obj",
+		platform,
+		"install",
+	).Dir(filepath.Join(workdir, "rumprun")).Run(); err != nil {
+		return err
+	}
+	return nil
+}
