a9800d573aec5c45f4aaf3efdced73d414b161a1
ilackarms <sdw35@cornell.edu>
2017-04-28 00:00:55 -0400

    add showCommand option

--- pkg/exec/exec.go
+++ pkg/exec/exec.go
@@ -11,6 +11,12 @@ import (
 
 type command struct {
 	*exec.Cmd
+	showCommand bool
+}
+
+func (cmd *command) ShowCommand(enable bool) *command {
+	cmd.showCommand = enable
+	return cmd
 }
 
 func (cmd *command) Dir(dir string) *command {
@@ -19,7 +25,9 @@ func (cmd *command) Dir(dir string) *command {
 }
 
 func (cmd *command) Run() error {
-	fmt.Fprintf(cmd.Stdout, "running command: %v\n", cmd.Args)
+	if cmd.showCommand {
+		fmt.Fprintf(cmd.Stdout, "running command: %v\n", cmd.Args)
+	}
 	if err := cmd.Cmd.Run(); err != nil {
 		return errors.New("failed running command "+strings.Join(cmd.Args, " "), err)
 	}
@@ -31,7 +39,10 @@ func Command(out io.Writer, args ...string) *command {
 	cmd.Args = args
 	cmd.Stdout = out
 	cmd.Stderr = out
-	return &command{cmd}
+	return &command{
+		Cmd:         cmd,
+		showCommand: true,
+	}
 }
 
 func RunCommand(out io.Writer, args ...string) error {
